388184b31afd992ced8ebc8816f8f316be99d89f
Federico Benitez <federicobenitez@gmail.com>
2024-09-08 13:33:00 -0300

    deployment working e2e

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ WORKDIR /calculator_api/
 RUN apt-get update -y ;\
     apt-get upgrade -y
 
+COPY /lib/pip.conf /etc/pip.conf
+
 RUN pip3 install \
          fastapi \
          uvicorn[standard] \
@@ -14,7 +16,7 @@ RUN pip3 install \
          redis \
          psutil
 
-COPY ./main/api.py .
+COPY /src/api.py .
 
 CMD ["uvicorn", "api:app", "--host", "0.0.0.0", "--port", "80"] 
 
--- src/api.py
+++ src/api.py
@@ -1,28 +1,8 @@
+"""ml-challenge-api"""
+
 from fastapi import FastAPI
 from prometheus_fastapi_instrumentator import Instrumentator
-import redis
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 import psutil
-
-try:
-    uri = 'mongodb+srv://nomoreops_mongo_sa:1hl75TRa0BmrpCiy@nomoreops-mongo.vb3psxx.mongodb.net/?retryWrites=true&w=majority'
-    client = MongoClient(uri, server_api=ServerApi('1'))
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-
-try:
-    r = redis.Redis(
-        host='redis-14962.c114.us-east-1-4.ec2.cloud.redislabs.com',
-        port=14962,
-        password='SxYjSXczUARk1yQWxQ8QyKLaPq5H3aCu'
-    )
-    r.ping()
-    print('Connected to redis redis-14962.c114.us-east-1-4.ec2.cloud.redislabs.com')
-except Exception as e:
-    print(e)
 
 app = FastAPI()
 Instrumentator().instrument(app).expose(app)
@@ -32,17 +12,49 @@ def keepalive():
     """Alive"""
     return {"Alive!"}
 
-@app.get("/")
+@app.get("/list_devices")
 def list_devices():
+    """List Devices"""
     devices = psutil.disk_partitions(all=True)
-    for device in devices:
-        print(f"Device: {device.device}")
-        print(f"  Mountpoint: {device.mountpoint}")
-        print(f"  File system type: {device.fstype}")
-        print()
+    return devices
 
 
 
 
-if __name__ == "__main__":
-    list_devices()
+
+
+
+
+
+
+
+
+
+# import redis
+# from pymongo.mongo_client import MongoClient
+# from pymongo.server_api import ServerApi
+
+# try:
+#     uri = 'mongodb+srv://nomoreops_mongo_sa:1hl75TRa0BmrpCiy@nomoreops-mongo.vb3psxx.mongodb.net/?retryWrites=true&w=majority'
+#     client = MongoClient(uri, server_api=ServerApi('1'))
+#     client.admin.command('ping')
+#     print("Pinged your deployment. You successfully connected to MongoDB!")
+# except Exception as e:
+#     print(e)
+
+# try:
+#     r = redis.Redis(
+#         host='redis-14962.c114.us-east-1-4.ec2.cloud.redislabs.com',
+#         port=14962,
+#         password='SxYjSXczUARk1yQWxQ8QyKLaPq5H3aCu'
+#     )
+#     r.ping()
+#     print('Connected to redis redis-14962.c114.us-east-1-4.ec2.cloud.redislabs.com')
+# except Exception as e:
+#     print(e)
+
+# for device in devices:
+#     print(f"Device: {device.device}")
+#     print(f"  Mountpoint: {device.mountpoint}")
+#     print(f"  File system type: {device.fstype}")
+#     print()
