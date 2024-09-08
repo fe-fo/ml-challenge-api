c6f1f078a79acffcc169977932678e7162dc86c8
Federico Benitez <federicobenitez@gmail.com>
2024-09-08 16:39:10 -0300

    working first part of the api

--- src/api.py
+++ src/api.py
@@ -7,54 +7,26 @@ import psutil
 app = FastAPI()
 Instrumentator().instrument(app).expose(app)
 
-@app.get("/")
-def keepalive():
-    """Alive"""
-    return {"Alive!"}
-
-@app.get("/list_devices")
-def list_devices():
+@app.get("/list_devices/")
+def list_devices(mountpoint: str | None = None):
     """List Devices"""
-    devices = psutil.disk_partitions(all=True)
-    return devices
+    class DeviceObject:
+        def __init__(self, device, mountpoint, diskUsage):
+            self.device = device
+            self.mountpoint = mountpoint
+            self.diskUsage = diskUsage
 
+    devicesList = list()
+    devices = psutil.disk_partitions(all=False)
+    for item in devices:
+        devicesList.append(
+            DeviceObject(
+                item.device,
+                item.mountpoint, 
+                psutil.disk_usage(item.mountpoint)
+            ),
+        )
 
-
-
-
-
-
-
-
-
-
-
-
-# import redis
-# from pymongo.mongo_client import MongoClient
-# from pymongo.server_api import ServerApi
-
-# try:
-#     uri = 'mongodb+srv://nomoreops_mongo_sa:1hl75TRa0BmrpCiy@nomoreops-mongo.vb3psxx.mongodb.net/?retryWrites=true&w=majority'
-#     client = MongoClient(uri, server_api=ServerApi('1'))
-#     client.admin.command('ping')
-#     print("Pinged your deployment. You successfully connected to MongoDB!")
-# except Exception as e:
-#     print(e)
-
-# try:
-#     r = redis.Redis(
-#         host='redis-14962.c114.us-east-1-4.ec2.cloud.redislabs.com',
-#         port=14962,
-#         password='SxYjSXczUARk1yQWxQ8QyKLaPq5H3aCu'
-#     )
-#     r.ping()
-#     print('Connected to redis redis-14962.c114.us-east-1-4.ec2.cloud.redislabs.com')
-# except Exception as e:
-#     print(e)
-
-# for device in devices:
-#     print(f"Device: {device.device}")
-#     print(f"  Mountpoint: {device.mountpoint}")
-#     print(f"  File system type: {device.fstype}")
-#     print()
+    if mountpoint:
+        return list(filter(lambda DeviceObject: DeviceObject.mountpoint == mountpoint, devicesList))
+    return devicesList
