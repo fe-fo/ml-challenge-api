db7a75ebea32e09ae4bbde88347a6fda9aa411cb
Federico Benitez <federicobenitez@gmail.com>
2024-09-16 13:35:18 -0300

    working

--- src/api.py
+++ src/api.py
@@ -6,19 +6,25 @@ from ..scripts.module import clean_folder
 
 app = FastAPI()
 
+@app.get("/")
+def alive():
+    """Keep Alive"""
+    return "Alive!"
+
 @app.get("/list_devices/")
 def list_devices(mountpoint: str | None = None):
     """List Devices"""
     class DeviceObject:
-        def __init__(self, device, mountpoint, diskUsage):
+        """Device Object"""
+        def __init__(self, device, mountpoint, disk_usage):
             self.device = device
             self.mountpoint = mountpoint
-            self.diskUsage = diskUsage
+            self.disk_usage = disk_usage
 
-    devicesList = list()
+    devices_list = list()
     devices = psutil.disk_partitions(all=False)
     for item in devices:
-        devicesList.append(
+        devices_list.append(
             DeviceObject(
                 item.device,
                 item.mountpoint, 
@@ -27,9 +33,10 @@ def list_devices(mountpoint: str | None = None):
         )
 
     if mountpoint:
-        return list(filter(lambda DeviceObject: DeviceObject.mountpoint == mountpoint, devicesList))
-    return devicesList
+        return list(filter(lambda DeviceObject: DeviceObject.mountpoint == mountpoint, devices_list))
+    return devices_list
 
 @app.post("/clean_tmp")
 def empty_tmp():
+    """Clean /tmp Folder"""
     return clean_folder('/tmp')
